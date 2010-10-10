1ea703cae835a86f4718685c8d9cbe770e376a78
Joshua Nichols <josh@technicalpickles.com>
2010-10-09 22:34:39 -0400

    Shuffle, shuffle, shuffle.

--- lib/4tehlulz.rb
+++ lib/4tehlulz.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/mongomapper'
 require 'joint'
+require 'imgur'
 
 require 'tempfile'
 
@@ -10,6 +11,8 @@ require '4tehlulz/montage'
 class Lulz
   include MongoMapper::Document
   plugin Joint
+
+  key :imgur_url, String
 
   attachment :result
 end
@@ -40,7 +43,11 @@ module ForTehLulz
       fourth_panel = gallery_path(params[:fourth_panel])
 
       montage = Montage.go! first_panel, second_panel, third_panel, fourth_panel
-      lulz = Lulz.create! :result => montage
+
+      imgur = Imgur::API.new('73c65910d8f5fe205d986b8b9f932ea0')
+      uploaded_image = imgur.upload_file(montage.path)
+
+      lulz = Lulz.create! :result => montage, :imgur_url => uploaded_image['original_image']
 
       flash[:notice] = "Uploaded!"
       session[:last_lulz_id] = lulz.id
@@ -48,7 +55,7 @@ module ForTehLulz
     end
 
     def gallery_path(selection)
-      "gallery/#{selection}.jpg"
+      "public/gallery/#{selection}.jpg"
     end
 
     def newly_uploaded_path(selection)
