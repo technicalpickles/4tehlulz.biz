b442e03cc4c5648fe937684f89c81ca9dabc21a4
Joshua Nichols <josh@technicalpickles.com>
2011-02-05 10:23:55 -0500

    WIP.

--- Capfile
+++ Capfile
@@ -3,4 +3,4 @@ load 'deploy'
 $LOAD_PATH.unshift '../../railsmachine/moonshine/lib'
 require 'moonshine/capistrano_integration'
 
-server 'ec2-75-101-202-189.compute-1.amazonaws.com', :app, :db, :web, :primary => true
+server '4tehlulz.josh.managedmachine.com', :app, :db, :web, :primary => true
--- app/manifests/application_manifest.rb
+++ app/manifests/application_manifest.rb
@@ -1,5 +1,14 @@
 require 'moonshine'
+require 'moonshine/mongodb'
 
 class ApplicationManifest < Moonshine::Manifest::Sinatra
-  recipe :default_stack
+  include Moonshine::Mongodb
+  recipe :default_stack, :mongodb
+
+  def application_packages
+    # fixme, find what needs these
+    package 'libcurl4-openssl-dev', :before => exec('bundle install')
+  end
+
+  recipe :application_packages
 end
